735b15e76de120f87572e0036b1485291b4addd0
wuyuhao <admin@example.com>
2021-09-02 22:52:33 +0800

    add 76-minWindow

---
+++ LeetCode/209-minSubArrayLen.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        # 定义一个无限大的数
+        res = float("inf")
+        Sum = 0
+        index = 0
+        for i in range(len(nums)):
+            Sum += nums[i]
+            while Sum >= s:
+                res = min(res, i-index+1)
+                Sum -= nums[index]
+                index += 1
+        return 0 if res==float("inf") else res
--- LeetCode/977-sortedSquares.py
+++ LeetCode/977-sortedSquares.py
@@ -1,7 +1,41 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        a = []
-        for i in range(0,len(nums) - 1):
-            a[i] = nums[i] * nums[i]
-        return sorted(a)
+        n = len(nums)
+        res = [0] * n
+        low,high,k = 0, n - 1, n - 1
+        while low <= high:
+            ls = nums[low] ** 2
+            hs = nums[high] ** 2
+            if ls > hs:
+                res[k] = ls
+                low += 1
+            elif ls < hs:
+                res[k] = hs
+                high -= 1
+            k -= 1
+        return res
+    
+    def bobblesort(arr):
+        for i in range(0,len(arr)):
+            for j in range(0, len(arr) - i):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        return arr
+
+    def selectionsort(arr):
+        for i in range(0,len(arr) - 1):
+            min = i
+            for j in range(i, len(arr)):
+                if arr[j] < arr[min]:
+                    min = j
+            arr[i], arr[min] = arr[min], arr[i]
+        return arr
+
+    def insertionsort(arr):
+        for 
+    
+
+
+                
+
             
