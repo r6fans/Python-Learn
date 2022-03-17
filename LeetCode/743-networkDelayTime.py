10d86d919925c46d486cfb8b97910b48a07b0dcf
wuyuhao <admin@example.com>
2022-03-17 21:02:02 +0800

    add some solution

--- LeetCode/743-networkDelayTime.py
+++ LeetCode/743-networkDelayTime.py
@@ -18,3 +18,21 @@ class Solution:
 
         ans = max(dist)
         return ans if ans < float('inf') else -1
+
+#领接矩阵
+
+#领接矩阵数组，w[a] [b] = c 代表从A到B有权重为C的边
+#加变操作
+def add(a,b,c):
+    w[a][b] = c
+
+#邻接表
+#邻接表与数组存储单链表的实现一致(头插法)，这种存图方式又叫链式前向星图，适用于变数较少的稀疏图使用
+
+
+
+#Floyd(邻接矩阵)
+def networkDelayTimeFloyd(self, times: List[List[int]], n: int, k: int) -> int:
+    #初始化邻接矩阵
+    INF = 10 ** 9
+    edge = [[]]
