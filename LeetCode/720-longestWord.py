067e33ef40a392ed4db624f5d6761f921fb873e5
wuyuhao <admin@example.com>
2022-03-31 22:01:23 +0800

    add 720 simple solution

--- LeetCode/720-longestWord.py
+++ LeetCode/720-longestWord.py
@@ -1,3 +1,5 @@
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        return min([w for w in words if all(w[:i] in s for range(1,len(w)))] + [""], key=lambda x : (-len(x),x)) if (s := set(words)) else ""
+        return min([w for w in words if all(w[:i] in s for i in range(1, len(w)))] + [""], key = lambda x : (-len(x), x)) if (s := set(words)) else ""
+
+        
