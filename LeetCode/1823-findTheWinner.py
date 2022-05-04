d0d830347d2168d2b2ef31bab610b5e58e663e1b
r6fans <wsyh1996325@gmail.com>
2022-05-04 09:54:25 +0800

    add new solution of problem 1823

--- LeetCode/1823-findTheWinner.py
+++ LeetCode/1823-findTheWinner.py
@@ -1,4 +1,4 @@
-#约瑟夫环问题： f(n,k)表示第n个胜利者的下标位置, 则f(n,k)=(f(n-1,k) + k)%n
+#约瑟夫环问题： f(n,k)表示第n个胜利者的下标位置, 则f(n,k)=(f(n-1, k) + k) % n + 1
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         if n < 1 or k < 1:
@@ -10,5 +10,11 @@ class Solution:
                 return 2
         else:
             return (self.findTheWinner(n - 1, k) + k - 1) % n + 1
+    # 迭代
+    def findTheWinner_1(self, n: int, k: int) -> int:
+        ans = 0
+        for i in range(2, n + 1):
+            ans = (ans + k) % i
+        return ans + 1
         
 print(Solution().findTheWinner(10, 20))
