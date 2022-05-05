380eae1ede58f99c79d69e741f3fc8d7cc6486e7
wuyuhao <admin@example.com>
2022-05-05 22:47:58 +0800

    add correct solution

--- LeetCode/713-numSubarrayProductLessThanK.py
+++ LeetCode/713-numSubarrayProductLessThanK.py
@@ -1,16 +1,14 @@
 class Solution:
-    def numSubarrayProductLessThanK(self, nums: list[int], k: int) -> int:
-        win = []
-        left, right = 0,0
-        ans = 0
-        cur = 1
-        while right < len(nums) - 1:
-            win.append(nums[right])
-            right += 1
-            cur = cur * nums[right]
-            while cur >= k:
-                ans += 1
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        ans, left, cur = 0, 0, 1
+        for right, num in enumerate(nums):
+            cur *= num
+            # 当前到右指针的连乘太大，需要一直挪动左指针直到小于k
+            while left <= right and cur >= k:
+                cur //= nums[left]
                 left += 1
+            # 在left到right之间的i, nums[i:right+1]的连乘都满足小于k
+            ans += right - left + 1
         return ans
 
 print(Solution().numSubarrayProductLessThanK([10,5,2,6], 100))
