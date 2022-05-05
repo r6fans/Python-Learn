30e028a299796121967d5178ff61b52de397fb15
r6fans <wsyh1996325@gmail.com>
2022-05-05 18:06:14 +0800

    add error solution 713

--- LeetCode/713-numSubarrayProductLessThanK.py
+++ LeetCode/713-numSubarrayProductLessThanK.py
@@ -1,2 +1,16 @@
 class Solution:
-    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+    def numSubarrayProductLessThanK(self, nums: list[int], k: int) -> int:
+        win = []
+        left, right = 0,0
+        ans = 0
+        cur = 1
+        while right < len(nums) - 1:
+            win.append(nums[right])
+            right += 1
+            cur = cur * nums[right]
+            while cur >= k:
+                ans += 1
+                left += 1
+        return ans
+
+print(Solution().numSubarrayProductLessThanK([10,5,2,6], 100))
