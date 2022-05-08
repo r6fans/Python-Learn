b3bee7c045582276d1316a03172a685f5951477e
wuyuhao <admin@example.com>
2022-05-08 21:52:35 +0800

    add solution 442

---
+++ LeetCode/442-findDuplicates.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findDuplicates(self, nums):
+        ans = []
+        for num in nums:
+            if nums[abs(num) - 1] < 0:
+                ans.append(abs(num))
+            nums[abs(num) - 1] *= - 1
+        return ans
