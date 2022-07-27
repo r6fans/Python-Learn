df7b2a5892b90038b3d7630c9ccffaafff28cd39
r6fans <wsyh1996325@gmail.com>
2022-07-27 09:32:41 +0800

    add solution 53

--- LeetCode/53-maxSubArray.py
+++ LeetCode/53-maxSubArray.py
@@ -1,2 +1,16 @@
+# 动态规划法
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+    def maxSubArray(self, nums: list[int]) -> int:
+        dp = []
+        dp.append(nums[0])
+        result = dp[0]
+        for i in range(1, len(nums)):
+            dp.insert(i, max(dp[i - 1] + nums[i], nums[i]))
+            result = max(result, dp[i])
+        
+        return result
+
+
+
+if __name__ == '__main__':
+    print(Solution().maxSubArray([5,4,-1,7,8]))
