9d46c85d148c0f080a0a21b76a5d579c0a8633c8
r6fans <wsyh19960325@gmail.com>
2022-08-19 15:46:25 +0800

    add 239 solutions

--- LeetCode/239-amxSlidingWindow.py
+++ LeetCode/239-amxSlidingWindow.py
@@ -1,3 +1,34 @@
+class myqueue:
+    def __init__(self):
+        self.queue = []
+    
+    def pop(self, value):
+        if self.queue and value == self.queue[0]:
+            return self.queue.pop(0)
+    
+    def push(self, value):
+        while self.queue and value > self.queue[-1]:
+            self.queue.pop(-1)
+        self.queue.append(value)
+
+    def front(self) -> int:
+        return self.queue[0]
+    
+    def get_queue(self):
+        return self.queue
+    
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        queue = myqueue()
+
+        for i in range(k):
+            queue.push(nums[i])
         
+        result = []
+        result.append(queue.front())
+        for j in range(k, len(nums)):
+            queue.pop(nums[j - k])
+            queue.push(nums[j])
+            result.append(queue.front())
+
+        return result  
