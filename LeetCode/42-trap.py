59fda6c1895d7ba0de9486ca3da0e9178f623d5e
wuyuhao <admin@example.com>
2022-08-20 21:54:14 +0800

    fix 42 dynamic solution

--- LeetCode/42-trap.py
+++ LeetCode/42-trap.py
@@ -29,23 +29,18 @@ class Solution:
     #动态规划
     def trap_dynamic(self, height: List[int]) -> int:
         leftheight, rightheight = [0]*len(height), [0]*len(height)
-
-        leftheight[0] = height[0]
-
-        for i in range(1, len(height)):
-            leftheight[i]=max(leftheight[i - 1], height[i])
         
-        rightheight[-1] = height[-1]
-
-        for i in range(len(height) - 2, -1, -1):
-            rightheight[i] = max(rightheight[i + 1], height[i])
+        leftheight[0]=height[0]
+        for i in range(1,len(height)):
+            leftheight[i]=max(leftheight[i-1],height[i])
+        rightheight[-1]=height[-1]
+        for i in range(len(height)-2,-1,-1):
+            rightheight[i]=max(rightheight[i+1],height[i])
         
         result = 0
-
-        for i in range(0, len(height)):
-            sum = min(leftheight[i], rightheight[i] - height[i])
-            result += sum
-        
+        for i in range(0,len(height)):
+            summ = min(leftheight[i],rightheight[i])-height[i]
+            result += summ
         return result
 
 if __name__ == "__main__":
