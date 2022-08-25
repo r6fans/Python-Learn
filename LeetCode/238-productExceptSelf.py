5de9b493ceadc3a9c22b9eca9f5b29b303f72bdc
wuyuhao <admin@example.com>
2022-08-26 05:30:52 +0800

    add new 238 solution

--- LeetCode/238-productExceptSelf.py
+++ LeetCode/238-productExceptSelf.py
@@ -1,15 +1,24 @@
 class Solution:
-    def productExceptSelf(self, nums):
-        ans = []
-        for i in range(len(nums)):
-            tem = 1
-            for j in range(len(nums)):
-                if i == j:
-                    continue
-                tem = tem * nums[j]
-            ans.append(tem)
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+
+        L = [0] * (length + 10)
+        R = [0] * (length + 10)
+
+        L[0] = 1
+
+        R[length + 1] = 1
+
+        for i in range(1, length):
+            L[i] = L[i-1] * nums[i - 1]
         
-        return ans
+        for j in range(length, -1, -1):
+            R[j] = R[j + 1] * nums[j - 1]
+        
+        return [L[i - 1] * R[i + 1] for i in range(1, length + 1)]
+
+
+
 
 if __name__ == '__main__':
     print(Solution().productExceptSelf([1,2,3,4]))
