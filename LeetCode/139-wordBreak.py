ecb3accecb260d27eea4270378c24ea69b243129
r6fans <wsyh19960325@gmail.com>
2022-08-26 09:32:29 +0800

    add one solution of problem 139

--- LeetCode/139-wordBreak.py
+++ LeetCode/139-wordBreak.py
@@ -1,22 +1,48 @@
-class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        memo = [None]*len(s)
+#         memo = [None]*len(s)
 
-        # 以索引i为起始到末尾的字符串能否由字典组成
-        def dfs(i):
-            # 长度超过s,返回True(空字符能组成)
-            if i >= len(s): 
-                return True
-            # 存在以i为起始的递归结果
-            if memo[i] != None:
-                return memo[i]
-            # 递归
-            for j in range(i,len(s)):
-                if s[i:j+1] in wordDict and dfs(j+1):
-                    memo[i] = True
-                    return True
-            memo[i] = False
-            return False
+#         # 以索引i为起始到末尾的字符串能否由字典组成
+#         def dfs(i):
+#             # 长度超过s,返回True(空字符能组成)
+#             if i >= len(s): 
+#                 return True
+#             # 存在以i为起始的递归结果
+#             if memo[i] != None:
+#                 return memo[i]
+#             # 递归
+#             for j in range(i,len(s)):
+#                 if s[i:j+1] in wordDict and dfs(j+1):
+#                     memo[i] = True
+#                     return True
+#             memo[i] = False
+#             return False
         
-        return dfs(0)
+#         return dfs(0)
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: list(str)) -> bool:
+        length = len(s)
+        memo = [None] * length
+         
+        def canBreak(start):
+            if start >= length:
+                return True
+
+            if memo[start] != None:
+                return memo[strart]
+             
+            for i in range(start + 1 ,length + 1):
+                prefix = s[start:i]
+                if (prefix in wordDict) and canBreak(i):
+                    memo[start] = True
+                    return True
+            
+            memo[start] = False
+            return False
+            
+        return canBreak(0)
+
+if __name__ == "__main__":
+    print(Solution().wordBreak("a", ["a"]))
