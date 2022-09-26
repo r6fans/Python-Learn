16542604c5f3845c839bd8ea062c2bfa3d9b4388
wuyuhao <admin@example.com>
2022-09-26 22:36:03 +0800

    ADD SOLUTION

--- LeetCode/39-combinationSum.py
+++ LeetCode/39-combinationSum.py
@@ -1,2 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        def dfs(candidates, begin, size, path, res, target):
+            if target == 0:
+                res.append(path)
+                return
+
+            for index in range(begin, size):
+                residue = target - candidates[index]
+                if residue < 0:
+                    break
+
+                dfs(candidates, index, size, path + [candidates[index]], res, residue)
+
+        size = len(candidates)
+        if size == 0:
+            return []
+        candidates.sort()
+        path = []
+        res = []
+        dfs(candidates, 0, size, path, res, target)
+        return res
