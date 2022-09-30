449d56e850c5b83b773937070a086b5ee4337a79
r6fans <wsyh19960325@gmail.com>
2022-09-30 08:54:27 +0800

    add solution for problem 33

---
+++ LeetCode/33-search.py
@@ -0,0 +1,24 @@
+class Solution:
+    #暴力法
+    # def search(self, nums: List[int], target: int) -> int:
+    #     if target in set(nums):
+    #         return nums.index(target)
+        
+    #     return -1
+    
+    #二分法
+    def search(self, nums: list(int), target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        mid = (low + high) / 2
+        if ((nums[0] > target) ^ (nums[0] > nums[mid]) ^ (target > nums[mid])):
+            low = mid + 1
+        else:
+            high = mid
+        
+        return (low == high & low if nums[low] == target else -1)
+    
+
+if __name__ == '__main__':
+    print(Solution().search([4,5,6,7,0,1,2], 0))
+            
